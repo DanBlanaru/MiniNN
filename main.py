99ab4f8caa936a01dba958a40a02055c90163c8d
Alexandru Virvarei <avirvarei@avirvarei-mac.am.ias.bitdefender.biz>
2020-01-17 11:14:15 +0200

    modular split

--- main.py
+++ main.py
@@ -1,301 +1,24 @@
 import numpy as np
 import gzip
 import pickle
-import math
 import activations as af
-import losses as ls
-import helpers as hp
+import losses
+import helpers
+import optimizers
+import layers
 from os import path, getcwd
-
-
-class Optimizer:
-    def __init__(self):
-        self.iterations = 0
-
-    def increment_iterations(self):
-        self.iterations += 1
-
-
-class SGD(Optimizer):
-    def __init__(self, lr=0.01, decay=0, momentum=0, nesterov=False):
-        super().__init__()
-        self.lr = lr
-        self.decay = decay
-        self.momentum = momentum
-        self.nesterov = nesterov
-        self.moments = []
-
-    def compile(self, total_weights):
-        self.moments = [[np.zeros(weights.shape) for weights in weights_per_layer] for weights_per_layer in
-                        total_weights]
-
-    def recalculate_weights(self, total_weights, total_gradients):
-        new_weights = []
-        if self.decay > 0:
-            self.lr *= (1 / (1 + self.decay * self.iterations))
-
-        for i, (weights_per_layer, gradients_per_layer) in enumerate(
-                zip(total_weights, total_gradients)):  # iterate throuh layers
-            new_weights_per_layer = []
-            for j, (weights, gradients) in enumerate(
-                    zip(weights_per_layer, gradients_per_layer)):  # weights then biases
-                self.moments[i][j] = self.momentum * self.moments[i][j]
-                self.moments[i][j] -= self.lr * gradients
-                if self.nesterov:
-                    curr_new_weights = weights + self.momentum * self.moments[i][j] - self.lr * gradients[j]
-                else:
-                    curr_new_weights = weights + self.moments[i][j]
-                new_weights_per_layer.append(curr_new_weights)
-            new_weights.append(new_weights_per_layer)
-        return new_weights
-
-
-class RMSProp(Optimizer):
-
-    def __init__(self, learning_rate=0.001, decay=0., rho=0.9, epsilon=1e-07):
-        super().__init__()
-        self.lr = learning_rate
-        self.decay = decay
-        self.rho = rho
-        self.epsilon = epsilon
-        self.moments = []
-
-    def compile(self, total_weights):
-        self.moments = [[np.zeros(weight.shape)
-                         for weight in weights] for weights in total_weights]
-
-    def recalculate_weights(self, total_weights, total_gradients):
-        new_weights = []
-        lr = self.lr
-        if self.decay > 0:
-            lr *= (1 / (1 + self.decay * self.iterations))
-        for i, (weights_per_layer, gradients_per_layer) in enumerate(
-                zip(total_weights, total_gradients)):  # iterate throuh layers
-            new_weights_per_layer = []
-            for j, (weights, gradients) in enumerate(
-                    zip(weights_per_layer, gradients_per_layer)):  # weights then biases
-                self.moments[i][j] = self.rho * self.moments[i][j] + \
-                                     (1. - self.rho) * np.square(gradients)
-                curr_new_weights = weights - lr * gradients / \
-                                   (np.sqrt(self.moments[i][j]) + self.epsilon)
-                new_weights_per_layer.append(curr_new_weights)
-            new_weights.append(new_weights_per_layer)
-        return new_weights
-
-
-class Rrop(Optimizer):
-
-    def __init__(self, incFactor=1.2, decFactor=0.5, stepSizeMax=50, stepSizeMin=1e-6):
-        super().__init__()
-        self.incFactor = incFactor
-        self.decFactor = decFactor
-        self.stepSizeMax = stepSizeMax
-        self.stepSizeMin = stepSizeMin
-        self.steps = []
-        self.signs = []
-
-    def compile(self, total_weights):
-        self.steps = [[np.zeros(weight.shape) + 0.001
-                       for weight in weights] for weights in total_weights]
-        self.signs = [[np.ones(weight.shape)
-                       for weight in weights] for weights in total_weights]
-
-    def recalculate_weights(self, total_weights, total_gradients):
-        new_weights = []
-
-        for i, (weights_per_layer, gradients_per_layer) in enumerate(
-                zip(total_weights, total_gradients)):  # iterate throuh layers
-            new_weights_per_layer = []
-            for j, (weights, gradients) in enumerate(
-                    zip(weights_per_layer, gradients_per_layer)):  # weights then biases
-
-                if gradients * self.signs[i][j] == 1:
-                    self.steps[i][j] = min(self.stepSizeMax, self.steps[i][j] * self.incFactor)
-                else:
-                    self.steps[i][j] = max(self.stepSizeMin, self.steps[i][j] * self.decFactor)
-
-                self.signs[i][j] = np.sign(gradients)
-
-                curr_new_weights = weights - np.sign(total_gradients) * self.steps[i][j]
-
-                new_weights_per_layer.append(curr_new_weights)
-            new_weights.append(new_weights_per_layer)
-        return new_weights
-
-
-class Adagrad(Optimizer):
-    def __init__(self, learning_rate=0.001, decay=0.):
-        super().__init__()
-        self.lr = learning_rate
-        self.decay = decay
-        self.squared_gradients = []
-
-    def compile(self, total_weights):
-        self.squared_gradients = [[np.zeros(weights.shape) for weights in weights_per_layer]
-                                  for weights_per_layer in total_weights]
-
-    def recalculate_weights(self, total_weights, total_gradients):
-        new_weights = []
-        lr = self.lr
-        if self.decay > 0:
-            lr *= (1 / (1 + self.decay * self.iterations))
-        for i, (weights_per_layer, gradients_per_layer) in enumerate(
-                zip(total_weights, total_gradients)):  # iterate throuh layers
-            new_weights_per_layer = []
-            for j, (weights, gradients) in enumerate(
-                    zip(weights_per_layer, gradients_per_layer)):  # weights then biases
-                self.squared_gradients[i][j] += np.square(gradients[i][j])
-                current_lr = self.lr / np.sqrt(self.squared_gradients[i][j])
-
-                curr_new_weights = weights - current_lr * gradients
-
-                new_weights_per_layer.append(curr_new_weights)
-            new_weights.append(new_weights_per_layer)
-        return new_weights
-
-
-
-class Adam(Optimizer):
-    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999):
-        super().__init__()
-        self.lr = learning_rate
-        self.beta1 = beta1
-        self.beta2 = beta2
-
-    def compile(self, total_weights):
-        self.moments = [[np.zeros(weights.shape) for weights in weights_per_layer]
-                                  for weights_per_layer in total_weights]
-        self.squared_gradients = [[np.zeros(weights.shape) for weights in weights_per_layer]
-                                  for weights_per_layer in total_weights]
-        self.t = 0
-    
-    def recalculate_weights(self, total_weights, total_gradients):
-        new_weights = []
-        lr = self.lr
-
-        for i, (weights_per_layer, gradients_per_layer) in enumerate(
-                zip(total_weights, total_gradients)):  # iterate throuh layers
-            new_weights_per_layer = []
-            for j, (weights, gradients) in enumerate(
-                    zip(weights_per_layer, gradients_per_layer)):  # weights then biases
-                self.moments[i][j] = self.beta1 * self.moments[i][j] + \
-                                (1. - self.beta1) * gradients
-
-                self.squared_gradients[i][j] = self.beta2 * self.squared_gradients[i][j] + \
-                                        (1. - self.beta2) * np.square(gradients)
-                self.t += 1
-
-                curr_momentum =self.moments[i][j] / (1 - self.beta1 ** self.t)
-                curr_squared_gradient = self.squared_gradients[i][j] / (1 - self.beta2 ** self.t)
-                
-                curr_new_weights = weights - lr * (curr_momentum / (np.sqrt(curr_squared_gradient) + 1e-6))
-
-                new_weights_per_layer.append(curr_new_weights)
-            new_weights.append(new_weights_per_layer)
-        return new_weights
-
-
-
-class Layer:
-    def __init__(self):
-        self.weights = []
-        self.gradients = []
-
-    def compile(self, input_size):
-        pass
-
-    def feed(self, input_):
-        pass
-
-    def backpropagate(self, z_errors):
-        pass
-
-    def get_weights(self):
-        return self.weights
-
-    def get_gradients(self):
-        return self.gradients
-
-
-class Dense(Layer):
-    def __init__(self, output_size, activation=af.identity):
-        super().__init__()
-        self.out_size = output_size
-        self.activation = activation
-        self.in_size = 0
-
-        self.input = []
-        self.z = []
-        self.y = []
-
-    def feed(self, input_):
-        self.input = input_
-        self.z = np.dot(self.input, self.weights[0]) + self.weights[1]
-        self.y = self.activation(self.z)
-        return self.y
-
-    def compile(self, input_size):
-        self.in_size = input_size
-        self.weights = [np.random.normal(0, math.sqrt(2 / self.in_size), size=(self.in_size, self.out_size)),
-                        np.zeros((1, self.out_size))]
-        self.gradients = [np.zeros((self.in_size, self.out_size)),
-                          np.zeros((1, self.out_size))]
-        return self.out_size
-
-    def backpropagate(self, y_errors, is_already_z=False):
-        if is_already_z:
-            z_errors = y_errors
-        else:
-            z_errors = y_errors * self.activation(self.y, is_derivative=True)
-        y_last_errors = np.dot(z_errors, np.transpose(self.weights[0]))
-
-        self.y_last_errors = np.copy(y_last_errors)
-        self.z_errors = np.copy(z_errors)
-
-        weight_grads = np.zeros(self.weights[0].shape)
-        bias_grads = np.zeros(self.weights[1].shape)
-        for z_iterator, y_iterator in zip(z_errors, self.input):
-            bias_grads += z_iterator
-            z_iterator = z_iterator.reshape(1, len(z_iterator))
-            y_iterator = y_iterator.reshape(len(y_iterator), 1)
-            w_grads_curr = np.dot(y_iterator, z_iterator)
-            weight_grads += w_grads_curr
-
-        weight_grads /= len(y_errors)
-        bias_grads /= len(y_errors)
-
-        self.gradients[0] = weight_grads
-        self.gradients[1] = bias_grads
-        return y_last_errors
-
-
-class Input(Layer):
-    def __init__(self, input_size, activation=af.identity):
-        super().__init__()
-
-        self.input_size = input_size
-        self.output_size = input_size
-        self.activation = activation
-        self.weights = []
-        self.gradients = []
-
-    def compile(self, _):
-        return self.output_size
-
-    def feed(self, input_):
-        return self.activation(input_)
 
 
 class Model:
     def __init__(self):
         self.layers = []
-        self.loss = ls.mse
-        self.optimizer = SGD()
+        self.loss = losses.mse
+        self.optimizer = optimizers.SGD()
 
     def add_layer(self, new_layer):
         self.layers.append(new_layer)
 
-    def compile(self, loss, optimizer=SGD()):
+    def compile(self, loss, optimizer=optimizers.SGD()):
         self.optimizer = optimizer
         self.loss = loss
 
@@ -319,7 +42,7 @@ class Model:
     def backpropagate(self, ytrue):
 
         if self.layers[-1].activation == af.softmax:
-            z_last_errors = ls.softmax_loss_derivative(self.layers[-1].y, ytrue)
+            z_last_errors = losses.softmax_loss_derivative(self.layers[-1].y, ytrue)
             last_errors = self.layers[-1].backpropagate(z_last_errors, True)
             for layer in reversed(self.layers[1:-1]):
                 last_errors = layer.backpropagate(last_errors)
@@ -335,7 +58,7 @@ class Model:
 
         for epoch_number in range(1, epochs + 1):
             print("epoch %d" % epoch_number)
-            minibatches = hp.make_minibatches(x, y, minibatch_size)
+            minibatches = helpers.make_minibatches(x, y, minibatch_size)
             for mini_x, mini_y in minibatches:
                 train_predicts = self.feed(mini_x)
 
@@ -368,10 +91,10 @@ class Model:
 
 if __name__ == "__main__":
     model = Model()
-    model.add_layer(Input(784))
-    model.add_layer(Dense(100, activation=af.relu))
-    model.add_layer(Dense(10, activation=af.softmax))
-    model.compile(ls.crossentropy, Adam())
+    model.add_layer(layers.Input(784))
+    model.add_layer(layers.Dense(100, activation=af.relu))
+    model.add_layer(layers.Dense(10, activation=af.softmax))
+    model.compile(losses.crossentropy, optimizers.Adam())
 
     # with gzip.open(path.join(getcwd(), 'data', 'mnist.pkl.gz'), 'rb') as f:
     with gzip.open('data/mnist.pkl.gz', 'rb') as f:
@@ -379,7 +102,7 @@ if __name__ == "__main__":
     n_train = train_set[0].shape[0]
     n_test = test_set[0].shape[0]
 
-    train_set_onehots = hp.make_onehot_2d(train_set[1], 10)
+    train_set_onehots = helpers.make_onehot_2d(train_set[1], 10)
     model.fit(train_set[0], train_set_onehots, 50, 50)
 
 # de schimbat pt regresie
